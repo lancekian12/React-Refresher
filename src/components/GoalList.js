e2aed82660a3804995c3b65fe5ac4c5d372a76fe
Lance Kian F. Flores <lafa.flores.up@phinmaed.com>
2025-05-04 00:22:11 +0800

    rendering list of data using prop and key prop

--- src/components/GoalList.js
+++ src/components/GoalList.js
@@ -3,7 +3,13 @@ import "./GoalList.css";
 
 const GoalList = (props) => {
   console.log(props.goals);
-  return <ul className="goal-list"></ul>;
+  return (
+    <ul className="goal-list">
+      {props.goals.map((goal) => {
+        return <li key={goal.id}>{goal.text}</li>;
+      })}
+    </ul>
+  );
 };
 
 export default GoalList;
